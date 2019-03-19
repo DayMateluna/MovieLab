b3911bbb20353f06bbb44b576bea05459082459a
CamilaGuerrero <cguerreroverdejo@gmail.com>
2019-03-19 11:20:06 -0300

    creando carpetas contenedoras del proyecto

---
+++ test/data.spec.js
@@ -0,0 +1,31 @@
+global.window = global;
+global.assert = require('chai').assert;
+require('../src/data');
+require('./data.spec.js');
+
+
+describe('' ,()=>{
+
+    it('',()=>{
+
+    });
+
+    it('',()=>{
+
+    });
+
+
+})
+
+describe('' ,()=>{
+
+    it('',()=>{
+    
+    });
+    
+    it('',()=>{
+    
+    });
+    
+    
+})
